3530877b15738dbe4e07a95d9e4f0b60ddf92866
keyzaw <keyzaalbian413@gmail.com>
2026-01-12 19:54:31 +0700

    Add files via upload

---
+++ KELAS/Pak Tri/NetBeansProjects/BelajarJavaDasar/src/xrpl.java
@@ -0,0 +1,32 @@
+public class xrpl {
+    public static void main(String[] args) {
+       int Angka;
+       Angka=100;
+       
+       System.out.println("Angka");
+       System.out.println("Angka yang anda inputkan: "+Angka);
+       Angka=100;
+       System.out.println("Angka terbaru adalah: "+Angka);
+       
+       double desimal=10.355;
+       float pecahan=13.4555f;
+        System.out.println(desimal);
+        System.out.println("Angka pecahan yang diinput: "+pecahan);
+        
+       String nama= "Keyza";
+       char abjad= 'A';
+        System.out.println(nama);
+        System.out.println(abjad);
+        
+       boolean benar = true;
+       boolean salah = false;
+        System.out.println(benar);
+        System.out.println(salah);
+        
+        desimal=100.234;
+        System.out.println(desimal);
+        
+       
+}
+        
+}
